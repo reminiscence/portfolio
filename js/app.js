1ea133182e13a77e145a71c6592ce741b32bd3b7
jeonguk <newlife8837@gmail.com>
2012-09-11 16:30:33 +0900

    update

---
+++ js/app.js
@@ -0,0 +1,71 @@
+$(function(){
+	var myInfo = info,
+		pg = program,
+		mySlide = slide,
+		$content = $('#content');
+		$contentBtn = $('#btn-info'),
+		$listbox = $('#listbox');
+	
+	//초기화
+	Init();
+
+	function Init(){
+		$('#title').fadeIn(1000);
+
+		$.get('./jst/info_template.jst',function(tmpl){
+			$.tmpl(tmpl, myInfo).appendTo($content);
+			$content.hide();
+			$content.fadeIn(1000);
+		});
+
+		$.get('./jst/program_picture.jst', function(tmpl){
+			$.tmpl(tmpl, pg).appendTo($content);
+			$content.fadeIn(1000);
+		});
+	}	
+
+	function viewSlide(cls){
+		$listbox.empty();
+		var str = cls;
+
+		$.get('./jst/'+str+'.jst', function(tmpl){
+			$.tmpl(tmpl, mySlide).appendTo($listbox);
+			$listbox.fadeIn(1000);			
+			
+			$('#closeListBox').click(function(){
+				$listbox.fadeOut(1000);
+			});
+		});
+	}
+
+	
+	$content.on('click', '.exhibition', function(e){
+		var cls = $(e.currentTarget).attr('class');
+		viewSlide(cls);
+	});
+
+	$content.on('click', '.scv_news_pic', function(e){
+		var cls = $(e.currentTarget).attr('class');
+		viewSlide(cls);
+	});
+	
+	$content.on('click', '.smart_book', function(e){
+		var cls = $(e.currentTarget).attr('class');
+		viewSlide(cls);
+	});
+	
+	$content.on('click', '.smart_book_ver', function(e){
+		var cls = $(e.currentTarget).attr('class');
+		viewSlide(cls);
+	});
+
+	$content.on('click', '.labAct', function(e){
+		var cls = $(e.currentTarget).attr('class');
+		viewSlide(cls);
+	});
+	
+	$content.on('click', '.nxcAct', function(e){
+		var cls = $(e.currentTarget).attr('class');
+		viewSlide(cls);
+	});
+});
---
+++ js/information.js
@@ -0,0 +1,51 @@
+var info = {
+	src : "./img/jeonguk.jpg",
+	name : "고정욱",
+	engName : "Ko Jeong Uk",
+	date : "1988.03.07",
+	address : "제주시 이도2동 1074-4번지",
+	email : "newlife8837@gmail.com",
+	programming : "javascript, PHP, jQuery"
+};
+
+var program = {
+	srcSmartB : "./img/smartbook/smartB.png",
+	srcSmartBV : "./img/smartbv/smartBV.png",
+	srcScv : "./img/scv/main.png",
+	srcExhibit : "./img/exhibit/exhibition.png",
+	srcLabActivity : "./img/lnsAct/lnsAct1.jpg",
+	srcNxcActivity : "./img/nxcAct/nxcAct2.jpg"
+};
+
+var slide = {
+	exhibition : {
+		src : ["./img/exhibit/exhibition.png", "./img/exhibit/exhibition1.png", "./img/exhibit/exhibition2.png"],
+		title : "[컴퓨터 교육과 전시회 - 교육부장]",
+		contents : "2011 컴퓨터교육과 전시회(11.11.25~11.11.26)입니다."
+	},
+	scv : {
+		src : ["./img/scv/main.png","./img/scv/article.png","./img/scv/list.png","./img/scv/cp.png","./img/scv/config.png"],
+		title :["SCV-news[메인화면]", "[기사보기]","[뉴스 리스트]","[뉴스사 선택]","[설정]"],
+		contents : ["뉴스와 제목, 날짜, 뉴스사 등의 정보가 보여집니다.", "현재 뉴스의 기사내용을 불러옵니다.","현재 뉴스사의 뉴스목록을 불러옵니다.","뉴스사를 선택할 수 있습니다.","자동재생 설정을 바꾸는 메뉴입니다."]
+	},
+	smartbook : {
+		src :["./img/smartbook/smartB.png"],
+		title : "올레 스마트북",
+		contents : "올레차에 대한 설명과 함께 사용자 간의 의견을 공유할 수 있는 어플리케이션입니다."
+	},
+	smartbookv : {
+		src : ["./img/smartbv/smartBV.png"],
+		title : ["올레스마트북 ver 2.0[메인]","올레스마트북 ver 2.0[지도]","올레스마트북 ver 2.0[게임]"],
+		contents : ["올레스마트북 메인화면입니다. UI와 디자인에 조금 더 신경썼습니다.", "부모층 사용자에게 자신의 위치에서 주변 관광지의 정보를 주는 기능입니다.", "어린이 사용자가 할 수 있는 올레차 관련 간단한 슈팅게임입니다."]
+	},
+	labAct : {
+		src : ["./img/lnsAct/lnsAct1.jpg","./img/lnsAct/lnsAct2.jpg"],
+		title : "Lns Lab - 연구실 세미나(연구 활동)",
+		contents : "방학동안 진행되는 연구실 세미나 발표입니다."
+	},
+	nxcAct : {
+		src : ["./img/nxcAct/nxcAct1.jpg","./img/nxcAct/nxcAct2.jpg","./img/nxcAct/nxcAct3.jpg","./img/nxcAct/nxcAct4.jpg"],
+		title : "교육활동 - 영재교육 조교(레고 Mindstorm NXC를 통한 프로그래밍 교육)",
+		contents : "레고 마인드스톰 NXC 를 통해 중등 정보과학반 영재교육 조교을 담당하였습니다."
+	}
+}
