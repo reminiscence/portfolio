0306bf21bc2cfd3d6b1f0074e6352ed958e0407e
jeonguk <newlife8837@gmail.com>
2013-06-19 14:21:43 +0900

    commit
    2013 06 19 first commit

--- js/information.js
+++ js/information.js
@@ -1,10 +1,10 @@
 var info = {
 	src : "./img/jeonguk.jpg",
 	name : "고정욱",
-	engName : "Ko Jeong Uk",
+	engName : "Jeong Uk Ko",
 	date : "1988.03.07",
 	email : "newlife8837@gmail.com",
-	programming : "javascript, PHP, jQuery"
+	programming : "C, Java, javascript, PHP"
 };
 
 var program = {
@@ -28,14 +28,14 @@ var slide = {
 		contents : ["뉴스와 제목, 날짜, 뉴스사 등의 정보가 보여집니다.", "현재 뉴스의 기사내용을 불러옵니다.","현재 뉴스사의 뉴스목록을 불러옵니다.","뉴스사를 선택할 수 있습니다.","자동재생 설정을 바꾸는 메뉴입니다.","북마크를 통해 원하는 뉴스를 골라봅니다."]
 	},
 	smartbook : {
-		src :["./img/smartbook/smartB.png"],
-		title : "올레 스마트북",
-		contents : "올레차에 대한 설명과 함께 사용자 간의 의견을 공유할 수 있는 어플리케이션입니다."
+		src :["./img/smartbook/sanhak.jpg", "./img/smartbook/fusion.jpg", "./img/smartbook/icct.jpg", "./img/smartbook/paper1.png"],
+		title :["한국산학기술학회 춘계학술대회 논문 제출", "한국융합학회 하계학술대회 논문 제출", "2012 ICCT 논문제출", "2013 ACM RACS 논문 제출"],
+		contents : ["제주 특산 올레차를 위한 스마트북의 설계 - 우수논문상 수상", "제주 특산 올레차 개발과 스마트북의 설계 - 우수논문상 수상", "Smart Contents of Jeju Olle Tea for Revitalization of Jeju Water Industry", "Measuring Similarity of Android Applications via Reversing and K-gram Birthmarking"]
 	},
 	smartbookv : {
-		src : ["./img/smartbv/smartBV.png", "./img/smartbv/smartBV1.png", "./img/smartbv/smartBV2.png"],
-		title : ["올레스마트북 ver 2.0[메인]","올레스마트북 ver 2.0[지도]","올레스마트북 ver 2.0[게임]"],
-		contents : ["올레스마트북 메인화면입니다. UI와 디자인에 조금 더 신경썼습니다.", "부모층 사용자에게 자신의 위치에서 주변 관광지의 정보를 주는 기능입니다.", "어린이 사용자가 할 수 있는 올레차 관련 간단한 슈팅게임입니다."]
+		src : ["./img/smartbook/smartB.png", "./img/smartbv/smartBV.png", "./img/smartbv/smartBV1.png", "./img/smartbv/smartBV2.png"],
+		title : [ "올레 스마트북", "올레스마트북 ver 2.0[메인]","올레스마트북 ver 2.0[지도]","올레스마트북 ver 2.0[게임]"],
+		contents : ["올레차에 대한 설명과 함께 사용자 간의 의견을 공유할 수 있는 어플리케이션입니다.", "올레스마트북 메인화면입니다. UI와 디자인에 조금 더 신경썼습니다.", "부모층 사용자에게 자신의 위치에서 주변 관광지의 정보를 주는 기능입니다.", "어린이 사용자가 할 수 있는 올레차 관련 간단한 슈팅게임입니다."]
 	},
 	labAct : {
 		src : ["./img/lnsAct/lnsAct1.png","./img/lnsAct/lnsAct2.png"],
